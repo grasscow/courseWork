b33e64ea203cf79c7adce63ce7160a6cf96756f0
Alexey ALexandrov <aleksandrov1988@gmail.com>
2015-04-28 18:15:49 +0300

    Страны

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,14 @@
 class Country < ActiveRecord::Base
-  has_many :films, ->{ordering}, dependent: :destroy
+  has_many :films, -> { ordering }, dependent: :destroy
+
+  scope :ordering, -> { order(:name) }
+
+
+  def self.manage?(u)
+    u.try(:admin?)
+  end
+
+  def can_destroy?
+    films.blank?
+  end
 end
